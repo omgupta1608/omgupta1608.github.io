d7c51efa300a883bcfc1caa1a28140a1a5e82dcb
Om Gupta <omgupta1608@gmail.com>
2023-01-26 19:49:27 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,124 @@
+let resumeData = {
+  imagebaseurl: "https://omgupta1608.github.io/",
+  name: "Om Gupta",
+  role: "I play my ukelele when I can't solve the bug",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/om-gupta-aaa0981a8/",
+      className: "fa fa-linkedin",
+      title: "LinkedIn"
+    },
+    {
+      name: "github",
+      url: "http://github.com/omgupta1608",
+      className: "fa fa-github",
+      title: "Github"
+    },
+    {
+      name: "skype",
+      url: "mailto:omgupta1608@gmail.com",
+      className: "fa fa-envelope",
+      title: "Mail"
+    },
+    {
+      name: "resume",
+      url: "https://docs.google.com/document/d/1FL5hFSUvSuc-mM6eiUe0Mqp7HkN1AmB0/edit?usp=sharing&ouid=101914746150620933579&rtpof=true&sd=true",
+      className: "fa fa-file",
+      title: "Resume"
+    },
+  ],
+  aboutme:
+    "I am a software engineer based in New Delhi, focusing mainly on backend engineering. With almost 2 years of professional experience, I have worked with various technologies like NodeJs, Golang. I love working on things like GraphQL, Docker and Postgres Databases. I recently won a national level Hackathon called Smart India Hackathon where I led a team of 5 members and managed the overall development and presentation of our project.",
+  address: "New Delhi, India",
+  website: "https://omgupta1608.github.io",
+  education: [
+    {
+      UniversityName: "Maharaja Surajmal Insitute, New Delhi",
+      specialization: "Bachelors's in Computer Application",
+      MonthOfPassing: "July",
+      YearOfPassing: "2022",
+      Achievements: "Overall CGPA - 9.76",
+    },
+    {
+      UniversityName: "Army Public School, Lucknow",
+      specialization: "Senior Secondry",
+      MonthOfPassing: "March",
+      YearOfPassing: "2019",
+      Achievements: "Physics, Chemistry, Maths with Computer Science - 94.4%",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "BridgeUp Tech Private Limited",
+      specialization: "Software Engineer",
+      MonthOfLeaving: "Jun 2022",
+      YearOfLeaving: " - Present",
+      Achievements: "Mumbai, India",
+    },
+    {
+      CompanyName: "Diagright",
+      specialization: "Junior Software Engineer",
+      MonthOfLeaving: "Oct 2021",
+      YearOfLeaving: " - May 2022",
+      Achievements: "Gurugram, Haryana, India",
+    },
+    {
+      CompanyName: "Ewar Games",
+      specialization: "Software Engineer Intern",
+      MonthOfLeaving: "Dec 2020",
+      YearOfLeaving: " - Mar 2021",
+      Achievements: "Bangalore Urban, Karnataka, India",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
