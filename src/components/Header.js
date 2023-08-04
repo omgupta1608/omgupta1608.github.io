db2fdc59750f6a9718387eb154653537056f0373
Om Gupta <omgupta1608@gmail.com>
2023-08-04 20:34:46 +0530

    added blog link

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,7 @@ export default class Header extends Component {
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a  href="/blog">Blog</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
              <li><a className="smoothscroll" href="#resume">Resume</a></li>
                {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li>
